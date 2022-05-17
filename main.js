da3154d7a2c1796415798fa77dc0ebb01f5f3905
MGibson7 <MGibsonASC@gmail.com>
2022-05-17 11:00:06 -0400

    Basic functionality for game added

--- main.js
+++ main.js
@@ -7,11 +7,39 @@ let catQA5 = document.getElementById("questionFive");
 let catQA6 = document.getElementById("questionSix");
 let catQA7 = document.getElementById("questionSeven");
 let catQA8 = document.getElementById("questionEight");
+let catQA9 = document.getElementById("questionNine");
+let catQA10 = document.getElementById("questionTen");
+let catQA11 = document.getElementById("questionEleven");
+let catQA12 = document.getElementById("questionTwelve");
+let catQA13 = document.getElementById("questionThirteen");
+let catQA14 = document.getElementById("questionFourteen");
+let catQA15 = document.getElementById("questionFifteen");
+let catQA16 = document.getElementById("questionSixteen");
 
-let slots = [catQA1, catQA2, catQA3, catQA4, catQA5, catQA6, catQA7, catQA8]
+let reveal1 = document.getElementById("reveal1");
+let reveal2 = document.getElementById("reveal2");
+let reveal3 = document.getElementById("reveal3");
+let reveal4 = document.getElementById("reveal4");
+let reveal5 = document.getElementById("reveal5");
+let reveal6 = document.getElementById("reveal6");
+let reveal7 = document.getElementById("reveal7");
+let reveal8 = document.getElementById("reveal8");
+let reveal9 = document.getElementById("reveal9");
+let reveal10 = document.getElementById("reveal10");
+let reveal11 = document.getElementById("reveal11");
+let reveal12 = document.getElementById("reveal12");
+let reveal13 = document.getElementById("reveal13");
+let reveal14 = document.getElementById("reveal14");
+let reveal15 = document.getElementById("reveal15");
+let reveal16 = document.getElementById("reveal16");
+
+
+let reveals = [reveal1, reveal2, reveal3, reveal4, reveal5, reveal6, reveal7, reveal8, reveal9, reveal10, reveal11, reveal12, reveal13, reveal14, reveal15, reveal16]
+let slots = [catQA1, catQA2, catQA3, catQA4, catQA5, catQA6, catQA7, catQA8, catQA9, catQA10, catQA11, catQA12, catQA13, catQA14, catQA15, catQA16]
+let question = {};
+let answer = {};
 
 //fetching clues
-
 
 function gameBoard(){
     for (let slot in slots){
@@ -21,10 +49,26 @@ function gameBoard(){
     })
     .then(function(response) {
         slots[slot].innerText = `${response[0].category.title}`
-        console.log(response)
+        question[slot] = response[0].question;
+        answer[slot] = response[0].answer;
+        slots[slot].addEventListener('click', event =>{
+            slots[slot].innerText = `${response[0].question}`
+            reveals[slot].style.display = "grid";
+            reveals[slot].addEventListener('click', event =>{
+                reveals[slot].innerText = `${response[0].answer}`
+
+            })
+
+        })
 
     });
 
     }
 }
+
+//addEventListener for Selection
+
+
 gameBoard();
+
+
